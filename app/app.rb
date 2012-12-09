12af3528efc0621bd4179153cb726d145f0e895f
Tyrone Trevorrow <tyrone@sudeium.com>
2012-12-09 16:53:27 +1100

    Extended the App module to provide more directories

---
+++ app/app.rb
@@ -0,0 +1,32 @@
+module BubbleWrap::App
+
+  def application_support_path
+    app_support = library_path.stringByAppendingPathComponent 'Application Support'
+    fm = NSFileManager.new
+    unless fm.fileExistsAtPath appSupport
+      fm.createDirectoryAtPath(app_support, 
+        withIntermediateDirectories: false, 
+        attributes: nil, 
+        error: nil)
+    end
+    app_support
+  end
+
+  def cache_path
+    path = NSSearchPathForDirectoriesInDomains(NSCachesDirectory,
+      NSUserDomainMask, true)[0]
+    fm = NSFileManager.new
+    unless fm.fileExistsAtPath path
+      fm.createDirectoryAtPath(path, 
+        withIntermediateDirectories: false, 
+        attributes: nil, 
+        error: nil)
+    end
+    path
+  end
+
+  def library_path
+    NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, true)[0]
+  end
+
+end
