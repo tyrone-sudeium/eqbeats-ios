e54f471392c9bb72af265043f678872f1eede8f7
Tyrone Trevorrow <tyrone@sudeium.com>
2012-12-09 16:01:47 +1100

    Implemented some file and artwork logic into Track

--- app/model/track.rb
+++ app/model/track.rb
@@ -1,12 +1,30 @@
 class Track < EQBeats::ModelObject
   attr_accessor :title, :artist, :download
 
+  def file_path
+    return mp3 unless File.exists? cache_path
+    NSURL.fileURLWithPath(cache_path).absoluteString
+  end
+
   def mp3
     @download[:mp3]
   end
 
-  def art_link
+  # quality, :thumb, :medium, :full
+  def art_link(quality = :full)
+    case quality
+    when :full
+      "#{@link}/art"
+    when :medium
+      "#{@link}/art/medium"
+    when :thumb
+      "#{@link}/art/thumb"
+    end
     "#{@link}/art"
+  end
+
+  def cache_path
+    "#{App.cache_path}/track_#{id}.mp3"
   end
 
 end
